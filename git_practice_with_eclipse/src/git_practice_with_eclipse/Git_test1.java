1e77e4059e5730ff874c048a635a915333fcc496
suhyunshim <lisa9963suhyun@gmail.com>
2021-10-12 16:18:33 +0900

    한글 깨짐 오류

--- git_practice_with_eclipse/src/git_practice_with_eclipse/Git_test1.java
+++ git_practice_with_eclipse/src/git_practice_with_eclipse/Git_test1.java
@@ -2,8 +2,8 @@ package git_practice_with_eclipse;
 
 public class Git_test1 {
 	public static void main(String[] args) {
-		System.out.println("¡ÚHello Git");
-		System.out.println("¡ÚHello Git¡Ú*");
+		System.out.println("Hello Git");
+		System.out.println("Hello Git ¼öÁ¤");
 	}
 
 }
