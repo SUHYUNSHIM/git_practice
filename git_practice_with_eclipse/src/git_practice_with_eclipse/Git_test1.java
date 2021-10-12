5cbe9cf7a675911d7496d146e073c40b2bbc8522
suhyunshim <lisa9963suhyun@gmail.com>
2021-10-12 16:13:55 +0900

    commit++

--- git_practice_with_eclipse/src/git_practice_with_eclipse/Git_test1.java
+++ git_practice_with_eclipse/src/git_practice_with_eclipse/Git_test1.java
@@ -3,7 +3,7 @@ package git_practice_with_eclipse;
 public class Git_test1 {
 	public static void main(String[] args) {
 		System.out.println("¡ÚHello Git");
-		System.out.println("¡ÚHello Git¡Ú");
+		System.out.println("¡ÚHello Git¡Ú*");
 	}
 
 }
