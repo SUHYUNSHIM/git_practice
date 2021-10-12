f38429ffb869539cde78414a95d9dd18b0f7040b
suhyunshim <lisa9963suhyun@gmail.com>
2021-10-12 16:23:26 +0900

    +

--- git_practice_with_eclipse/src/git_practice_with_eclipse/Git_test1.java
+++ git_practice_with_eclipse/src/git_practice_with_eclipse/Git_test1.java
@@ -3,7 +3,7 @@ package git_practice_with_eclipse;
 public class Git_test1 {
 	public static void main(String[] args) {
 		System.out.println("Hello Git");
-		System.out.println("Hello Git ¼öÁ¤");
+		System.out.println("Hello Git new");
 	}
 
 }
